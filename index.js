8fa885e432b9ae40911da739e4fc5f8b055bb26d
rolins2 <127750238+rolins2@users.noreply.github.com>
2023-12-17 01:50:30 +0530

    Created header

--- index.js
+++ index.js
@@ -2,7 +2,12 @@ import express from "express";
 const app = express();
 
 const port = 3000;
+app.get("/",(req,res)=>{
+  res.render("index.ejs")
+})
 
 app.listen(port, () => {
   console.log(`Running on ${port}`);
 });
+
+
